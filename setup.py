eabd27a40379c9aff2899e4c6f0528bdd5eca4d0
Joshua Harlow <harlowja@yahoo-inc.com>
2014-05-17 21:11:28 -0700

    Bump for 0.1.7

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def _readme():
 
 
 setup(name='doc8',
-      version='0.1.6',
+      version='0.1.7',
       description='style checker for sphinx (or other) rst documentation.',
       url='https://github.com/harlowja/doc8',
       scripts=[

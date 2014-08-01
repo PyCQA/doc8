72d30dae0a27b52a910059534e158b9f85407266
Christian Berendt <berendt@b1-systems.de>
2014-08-01 15:12:33 +0200

    Make build environment workable
    * Add files needed to build documentation. They are copied
      from the cookiecutter repository.
    
    * Replace setup.py and add setup.cfg
    
    * Add tox.ini
    
    * Add requirements.txt and test-requirements.txt
    
    * Fix hacking issue H307 (like imports should be grouped together)
    
    Change-Id: I325bb07233853611e8f0952a554a30dba2b7dfe7
    Co-Authored-By: Joshua Harlow <harlowja@yahoo-inc.com>

--- setup.py
+++ setup.py
@@ -1,70 +1,30 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-#    Copyright (C) 2014 Yahoo! Inc. All Rights Reserved.
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#         http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import os
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import setuptools
 
-from setuptools import find_packages
-from setuptools import setup
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-
-def _path(fn):
-    return os.path.join(os.path.dirname(__file__), fn)
-
-
-def _readme():
-    with open(_path("README.rst"), "r") as handle:
-        return handle.read()
-
-
-setup(
-    name='doc8',
-    version='0.3.4',
-    description='style checker for sphinx (or other) rst documentation.',
-    url='http://git.openstack.org/cgit/stackforge/doc8',
-    license="ASL 2.0",
-    install_requires=[
-        'argparse',
-        'docutils',
-        'restructuredtext-lint>=0.7',
-        'six',
-        'stevedore',
-    ],
-    entry_points={
-        'console_scripts': [
-            'doc8 = doc8.main:main',
-        ],
-    },
-    packages=find_packages(),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-    ],
-    keywords="rst docs style checking",
-    long_description=_readme(),
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)

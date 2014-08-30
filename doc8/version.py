de557a68924450c43bd452f5799fd89c057758f8
Joshua Harlow <harlowja@gmail.com>
2014-08-30 10:05:42 -0700

    Add version argument and output
    Change-Id: I71c970c338297ada898fe5df887ecb975f7c3b29

---
+++ doc8/version.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright (C) 2014 Yahoo! Inc. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pkg_resources
+
+try:
+    _package_info = pkg_resources.get_distribution('doc8')
+except pkg_resources.DistributionNotFound:
+    _package_info = None
+
+
+def version_string():
+    if not _package_info:
+        return None
+    return _package_info.version

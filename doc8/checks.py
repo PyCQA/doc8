6aa20275adc1281aac20b3399b00caf4dec11e98
Anne Gentle <anne@openstack.org>
2015-02-27 18:03:42 -0600

    Adds 'linenos' directive to the ignore regex
    - Ideally we'll fix at the linter level (and probably start maintaining our
    own linter in openstack-doc-tools). Issue logged at
    https://github.com/twolfson/restructuredtext-lint/issues/11
    
    Change-Id: I9733aa81bd342aba4d37657ab9dd17dfca1ca13b
    Partial-bug: 1426503

--- doc8/checks.py
+++ doc8/checks.py
@@ -102,6 +102,7 @@ class CheckValidity(ContentCheck):
         re.compile(r'^Unknown directive type'),
         re.compile(r'^Undefined substitution'),
         re.compile(r'^Substitution definition contains illegal element'),
+        re.compile(r'^unknown option: \"linenos\"'),
     ]
 
     def __init__(self, cfg):

7366c5dcda1610f2e7ee43f545c3442cb50740d9
Sorin Sbarnea <ssbarnea@redhat.com>
2022-12-21 08:02:54 +0000

    Remove use of pbr

--- src/doc8/__init__.py
+++ src/doc8/__init__.py
@@ -1,1 +1,7 @@
-from .main import doc8  # noqa
+"""doc8 - A docutils linter."""
+from __future__ import annotations
+from doc8.version import __version__
+from doc8.main import doc8  # noqa
+
+
+__all__ = ("__version__",)
